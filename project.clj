bc63ab2428eaf7ee61035b95bd6c0b80cdf67fb0
Paul Holser <pholser@alumni.rice.edu>
2021-05-18 21:50:35 -0500

    add get endpoints

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
 		 [compojure "1.6.1"]
 		 [http-kit "2.3.0"]
 		 [ring/ring-defaults "0.3.2"]
-		 [org.clojure/data.json "0.2.6"]
+		 [metosin/jsonista "0.3.3"]
 		 [talltale "0.4.3"]]
   :main nil
   :target-path "target/%s"
--- src/records/rest.clj
+++ src/records/rest.clj
@@ -7,26 +7,37 @@
             [ring.middleware.defaults :refer :all]
             [clojure.pprint :as pp]
             [clojure.string :as str]
-            [clojure.data.json :as json]))
+            [jsonista.core :as json]))
 
-(defroutes app-routes
-  (POST "/records" [] add-record)
-  (GET "/records/color" [] records-by-color)
-  (GET "/records/birthdate" [] records-by-birthdate)
-  (GET "/records/color" [] records-by-name)
-  (route/not-found "error, no route"))
+(def records-store (atom (rec/file->records "random.csv")))
 
-(def records-store (atom (file->records "random.csv")))
+(defn records-sorted-by [criterion]
+  (->> @records-store
+    (sort-by criterion)
+    (json/write-value-as-string)))
 
-(def records-by-color [req]
+(defn records-json [recs]
   {:status 200
    :headers {"Content-Type" "application/json"}
-   :body (str (json/write-str @records-store))})
+   :body recs})
+  
+(defn records-by-color [req]
+  (records-json (records-sorted-by :favorite-color)))
+(defn records-by-birthdate [req]
+  (records-json (records-sorted-by :birthdate)))
+(defn records-by-name [req]
+  (records-json (records-sorted-by (juxt :last-name :first-name))))
+
+(defroutes app-routes
+;;  (POST "/records" [] add-record)
+  (GET "/records/color" [] records-by-color)
+  (GET "/records/birthdate" [] records-by-birthdate)
+  (GET "/records/name" [] records-by-name)
+  (route/not-found "error, no route"))
 
 (defn -main
   "Read files named in command line args, sort and print"
   [& args]
-  (println "boo...")
   (server/run-server (wrap-defaults #'app-routes api-defaults)
                      {:port 3000})
   (println (str "Running API on port 3000")))
