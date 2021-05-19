9bdcfef317a1c92f9a24cf4ae14a0fbdc277985f
Paul Holser <pholser@alumni.rice.edu>
2021-05-19 12:53:17 -0500

    test for post - need to do the +1 check on subsequent get

--- test/records/rest_test.clj
+++ test/records/rest_test.clj
@@ -29,3 +29,14 @@
       (is (= status 200))
       (is (= 300 (count cooked)))
       (is (= (sort-by :birthdate cooked) cooked)))))
+
+(deftest post-a-text-record
+  (testing "POST /records"
+    (let [p-resp
+            @(http/post "http://localhost:3000/records"
+              {:headers {"Content-type" "text/plain"}
+               :query-params {"type" "txt"}
+               :body "9000 HAL hal@hal.com pink 1992-01-12"})
+	  cooked-post (json/read-value (:body p-resp))]
+        (is (= (:status p-resp) 200))
+        (is (= "HAL" (get cooked-post "first-name"))))))
