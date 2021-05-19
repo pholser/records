f49e05f0682f488109b13a2a3cf5fc132435b472
Paul Holser <pholser@alumni.rice.edu>
2021-05-19 12:57:10 -0500

    tweak project file

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject records "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "Clojure REST API exploration"
+  :url "http://github.com/pholser/records"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [javax.mail/mail "1.4.3"]
                  [clojure.java-time "0.3.2"]
